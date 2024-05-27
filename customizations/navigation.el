7592a85b53dc5dd23045d4943dabdc1b90e67016
tulio.abner <tulio.abner@brasilparalelo.com.br>
2024-05-27 17:10:22 -0300

    fix: removing wrong keybiding

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -20,9 +20,7 @@
   (:option ivy-use-virtual-buffers t
            ivy-re-builders-alist '((t . ivy--regex-ignore-order))
            ivy-count-format "%d/%d ")
-  (:global "C-s" swiper
-           "s-f" swiper
-           "C-x C-f" counsel-find-file
+  (:global "C-x C-f" counsel-find-file
            "C-x C-b" counsel-switch-buffer
            "M-x" counsel-M-x))
 
