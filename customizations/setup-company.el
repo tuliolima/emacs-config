cb2376c32a41cbd9df395f3709aa6f8809ae9737
tulio.abner <tulio.abner@brasilparalelo.com.br>
2024-06-18 13:10:41 -0300

    feat: setting up company

--- customizations/setup-company.el
+++ customizations/setup-company.el
@@ -1,13 +1,15 @@
 (use-package company
+  :ensure t
   :after lsp-mode
-  :hook (lsp-mode . company-mode)
+  :hook (prog-mode . company-mode)
   :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
+         ("<tab>" . company-complete-selection)
+         :map lsp-mode-map
          ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
 
 (use-package company-box
+  :ensure t
   :hook (company-mode . company-box-mode))
