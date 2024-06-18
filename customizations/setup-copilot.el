f3acb06dfce159d642f0be7bee8e8a48fdec8444
tulio.abner <tulio.abner@brasilparalelo.com.br>
2024-06-18 13:11:44 -0300

    feat: setting up github copilot

--- customizations/setup-copilot.el
+++ customizations/setup-copilot.el
@@ -1,5 +1,17 @@
 (use-package copilot
+  :ensure t
   :quelpa (copilot :fetcher github
                    :repo "copilot-emacs/copilot.el"
                    :branch "main"
-                   :files ("*.el")))
+                   :files ("*.el"))
+  :hook (prog-mode . copilot-mode)
+  :custom
+  (copilot-node-executable "/home/tulio.abner/.nvm/versions/node/v20.13.1/bin/node")
+  (copilot-indent-offset-warning-disable t)
+  :bind (("C-SPC" . 'copilot-complete)
+         :map copilot-completion-map
+         ("<tab>" . 'copilot-accept-completion)
+         ("M-[" . 'copilot-previous-completion)
+         ("M-]" . 'copilot-next-completion)
+         ("C-<right>" . 'copilot-accept-completion-by-word)
+         ("C-g" . 'copilot-clear-overlay)))
