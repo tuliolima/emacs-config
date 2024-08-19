1d6e30f7f364c4493aaf70c74c25af59b77e50ab
tulio.abner <tulio.abner@brasilparalelo.com.br>
2024-08-19 06:33:22 -0300

    refact: setting up magit with use-package

--- customizations/git.el
+++ customizations/git.el
@@ -1,5 +1,5 @@
-;; magit is a full-fledged interface for git
-;; https://magit.vc/manual/magit/
 (add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
-(setup (:package magit)
-  (:global "C-M-;" magit-status))
+
+(use-package magit
+  :ensure t
+  :bind ("C-M-;" . magit-status) )
