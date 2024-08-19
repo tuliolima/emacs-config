fbe92b684f9aec65b7b3d6a9ab8164f9ea63420c
tulio.abner <tulio.abner@brasilparalelo.com.br>
2024-08-19 06:32:01 -0300

    feat: changing key for dired-up-directory

--- customizations/dired-config.el
+++ customizations/dired-config.el
@@ -1,4 +1,5 @@
 (define-key dired-mode-map (kbd "C-o") 'dired-create-empty-file)
+(define-key dired-mode-map (kbd "b") 'dired-up-directory)
 
 ;; Open dired folders in same buffer
 (put 'dired-find-alternate-file 'disabled nil)
