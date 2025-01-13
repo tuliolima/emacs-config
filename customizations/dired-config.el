1d274cc71894f113776c510fa17246f01a58e15d
Túlio Lima <tulioabner@gmail.com>
2025-01-13 17:40:39 -0300

    feat: using dired-subtree

--- customizations/dired-config.el
+++ customizations/dired-config.el
@@ -3,3 +3,10 @@
 
 ;; Open dired folders in same buffer
 (put 'dired-find-alternate-file 'disabled nil)
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
