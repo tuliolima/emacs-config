bd3b1599c7e02a57708d993548ada35a635071d0
Túlio Lima <tulioabner@gmail.com>
2025-01-24 12:21:59 -0300

    feat: showing readme text in dired

--- customizations/dired-config.el
+++ customizations/dired-config.el
@@ -13,3 +13,21 @@
 
 ;; Activate dired-hide-details-mode by default
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+
+(defun my-dired-show-readme ()
+  "Show the content of README.md in the dired buffer if it exists."
+  (when (and (eq major-mode 'dired-mode)
+             (not (file-remote-p default-directory))) ;; Avoid remote directories
+    (let* ((files (directory-files default-directory))
+           (readme-file (seq-find (lambda (f)
+                                    (string-match-p "\\`readme\\.md\\'" (downcase f)))
+                                  files)))
+      (when readme-file
+        (let ((inhibit-read-only t)) ;; Temporarily allow buffer modification
+          (save-excursion
+            (goto-char (point-max))
+            (insert "\n\n")
+            (insert-file-contents readme-file)
+            (message "Inserted README.md at the end of the buffer.")))))))
+
+(add-hook 'dired-after-readin-hook 'my-dired-show-readme)
